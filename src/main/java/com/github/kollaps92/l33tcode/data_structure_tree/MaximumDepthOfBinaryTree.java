90ce56f0b0f9e3383d78a6890cfaff33b1a71a9d
Anton B <anton.kta@gmail.com>
2021-04-05 19:57:03 +0300

    added solution to find Max Depth for Binary tree. Bottom-up

---
+++ src/main/java/com/github/kollaps92/l33tcode/data_structure_tree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,28 @@
+package com.github.kollaps92.l33tcode.data_structure_tree;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
