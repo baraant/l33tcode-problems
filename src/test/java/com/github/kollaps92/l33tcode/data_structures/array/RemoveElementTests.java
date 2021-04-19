62736070d399c1719e86106a463996993fcb2b0a
Anton B <anton.kta@gmail.com>
2021-04-19 10:15:56 +0300

    Added RemoveElement solution + tests

---
+++ src/test/java/com/github/kollaps92/l33tcode/data_structures/array/RemoveElementTests.java
@@ -0,0 +1,34 @@
+package com.github.kollaps92.l33tcode.data_structures.array;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+public class RemoveElementTests {
+
+    RemoveElement re = new RemoveElement();
+
+    @Test
+    public void test() {
+        int[] nums = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8).toArray();
+
+        int resultedLength = re.removeElement(nums, 2);
+        System.out.println("Hi");
+    }
+
+    @Test
+    public void test_2() {
+        int[] nums = IntStream.of(2).toArray();
+
+        int resultedLength = re.removeElement(nums, 2);
+        System.out.println("Hi");
+    }
+
+    @Test
+    public void test_3() {
+        int[] nums = IntStream.of(2, 2, 0, 2).toArray();
+
+        int resultedLength = re.removeElement(nums, 2);
+        System.out.println("Hi");
+    }
+}
