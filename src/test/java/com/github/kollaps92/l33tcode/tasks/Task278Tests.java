f0e4ee28f5e07f93c18c7671089afd178ae3c351
Anton B <anton.kta@gmail.com>
2022-01-17 16:37:57 +0300

    Added Task 278 + test

---
+++ src/test/java/com/github/kollaps92/l33tcode/tasks/Task278Tests.java
@@ -0,0 +1,13 @@
+package com.github.kollaps92.l33tcode.tasks;
+
+import org.junit.jupiter.api.Test;
+
+public class Task278Tests {
+
+    @Test
+    public void test278_1() {
+        Task278 task278 = new Task278();
+
+        System.out.println(task278.firstBadVersion(5));
+    }
+}
