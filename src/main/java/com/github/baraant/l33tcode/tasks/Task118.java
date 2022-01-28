493810d137e4a28bf0464eb780ddf3224a0e602a
Anton B <anton.kta@gmail.com>
2022-01-28 23:02:07 +0300

    Added task 118 + tests

---
+++ src/main/java/com/github/baraant/l33tcode/tasks/Task118.java
@@ -0,0 +1,61 @@
+package com.github.baraant.l33tcode.tasks;
+
+import java.util.*;
+
+/*
+118. Pascal's Triangle
+Easy
+
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+Example 1:
+
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Example 2:
+
+Input: numRows = 1
+Output: [[1]]
+
+
+Constraints:
+
+1 <= numRows <= 30
+*/
+public class Task118 {
+    public List<List<Integer>> generate(int numRows) {
+        List<Integer> firstRow = Arrays.asList(1);
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(firstRow);
+
+        List<Integer> curRow = firstRow;
+
+        for (int i = 1; i< numRows; i++) {
+            List<Integer> nextRow = generateNextRow(curRow);
+            result.add(nextRow);
+            curRow = nextRow;
+        }
+        return result;
+    }
+
+    private List<Integer> generateNextRow(List<Integer> prevRow) {
+        List<Integer> result = new ArrayList<>();
+
+        Iterator<Integer> iterator = prevRow.iterator();
+
+        int prevValue = 0;
+        int curValue;
+
+        do {
+            curValue = iterator.next();
+            result.add(prevValue + curValue);
+            prevValue = curValue;
+        } while (iterator.hasNext());
+
+        result.add(prevValue);
+
+        return result;
+    }
+}
---
+++ src/test/java/com/github/baraant/l33tcode/tasks/Task118Tests.java
@@ -0,0 +1,13 @@
+package com.github.baraant.l33tcode.tasks;
+
+import org.junit.jupiter.api.Test;
+
+public class Task118Tests {
+
+    @Test
+    public void test118_1() {
+        Task118 task118 = new Task118();
+
+        System.out.println(task118.generate(30));
+    }
+}
