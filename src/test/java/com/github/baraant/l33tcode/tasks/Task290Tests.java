9adda2a3580800da5c18c6ba603a4724053e8acc
Anton B <anton.kta@gmail.com>
2025-06-29 00:25:59 +0200

    wordPattern solution

---
+++ src/test/java/com/github/baraant/l33tcode/tasks/Task290Tests.java
@@ -0,0 +1,13 @@
+package com.github.baraant.l33tcode.tasks;
+
+import org.junit.jupiter.api.Test;
+
+public class Task290Tests {
+
+    @Test
+    public void test290_1() {
+        Task290 task290 = new Task290();
+
+        System.out.println(task290.wordPattern("abba", "dog dog dog dog"));
+    }
+}
