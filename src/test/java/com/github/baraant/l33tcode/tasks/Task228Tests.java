714e002edc7f43b1fc6b04a21f6a1f7eb015d9e3
Anton B <anton.kta@gmail.com>
2025-07-04 13:09:19 +0200

    summaryRanges solution

---
+++ src/test/java/com/github/baraant/l33tcode/tasks/Task228Tests.java
@@ -0,0 +1,16 @@
+package com.github.baraant.l33tcode.tasks;
+
+import org.junit.jupiter.api.Test;
+
+public class Task228Tests {
+
+    @Test
+    public void test228_1() {
+        Task228 task228 = new Task228();
+
+        int[] nums = new int[]{0, 1, 2, 4, 5, 7};
+
+        task228.summaryRanges(nums);
+        System.out.println("hi");
+    }
+}
