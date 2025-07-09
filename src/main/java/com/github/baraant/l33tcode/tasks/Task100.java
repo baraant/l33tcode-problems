a39ed4e64dd251449fb25e668a0ce4ffbfd0a568
Anton B <anton.kta@gmail.com>
2025-07-09 17:38:51 +0200

    isSameTree solution

--- src/main/java/com/github/baraant/l33tcode/tasks/Task100.java
+++ src/main/java/com/github/baraant/l33tcode/tasks/Task100.java
@@ -1,29 +1,29 @@
 package com.github.baraant.l33tcode.tasks;
 
 /*
-226. Invert Binary Tree
-Given the root of a binary tree, invert the tree, and return its root.
+100. Same Tree
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
 
 Example 1:
 
-Input: root = [4,2,7,1,3,6,9]
-Output: [4,7,2,9,6,3,1]
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
 Example 2:
 
-Input: root = [2,1,3]
-Output: [2,3,1]
+Input: p = [1,2], q = [1,null,2]
+Output: false
 Example 3:
 
-Input: root = []
-Output: []
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
 
 Constraints:
 
-The number of nodes in the tree is in the range [0, 100].
--100 <= Node.val <= 100
+The number of nodes in both trees is in the range [0, 100].
+-10^4 <= Node.val <= 10^4
 */
-
-import com.sun.source.tree.Tree;
 
 /**
  * Definition for a binary tree node.
@@ -40,19 +40,16 @@ import com.sun.source.tree.Tree;
  * }
  * }
  */
-public class Task226 {
-    public TreeNode invertTree(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
+public class Task100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
 
-        TreeNode left = invertTree(root.left);
-        TreeNode right = invertTree(root.right);
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
 
-        root.left = right;
-        root.right = left;
+        if (!isSameTree(p.left, q.left)) return false;
+        if (!isSameTree(p.right, q.right)) return false;
 
-        return root;
+        return p.val == q.val;
     }
 
     private class TreeNode {
