bcec9a015c842e9a3edde797a7ec0b257f8d91b6
Anton B <anton.kta@gmail.com>
2025-07-17 22:48:00 +0200

    maxDepth solution

---
+++ src/main/java/com/github/baraant/l33tcode/tasks/Task104.java
@@ -0,0 +1,49 @@
+package com.github.baraant.l33tcode.tasks;
+
+/*
+104. Maximum Depth of Binary Tree
+Easy
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 10^4].
+-100 <= Node.val <= 100
+*/
+public class Task104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        return Math.max(1 + maxDepth(root.left), 1 + maxDepth(root.right));
+    }
+
+        private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
