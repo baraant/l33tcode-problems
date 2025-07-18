8afe2ba8e9b61dd880e3796440280f8085b33095
Anton B <anton.kta@gmail.com>
2025-07-18 17:51:33 +0200

    reverseWords solution

---
+++ src/test/java/com/github/baraant/l33tcode/tasks/Task151Tests.java
@@ -0,0 +1,13 @@
+package com.github.baraant.l33tcode.tasks;
+
+import org.junit.jupiter.api.Test;
+
+public class Task151Tests {
+
+    private Task151 task151 = new Task151();
+
+    @Test
+    public void test151_1() {
+        task151.reverseWords("the sky is blue");
+    }
+}
