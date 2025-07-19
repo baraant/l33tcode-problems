46a1ae78396d1fcc79ff2fc04be8a98436b2cfde
Anton B <anton.kta@gmail.com>
2025-07-19 16:20:53 +0200

    processStr solution

---
+++ src/test/java/com/github/baraant/l33tcode/contests/contest458w/Contest458wTask1Tests.java
@@ -0,0 +1,13 @@
+package com.github.baraant.l33tcode.contests.contest458w;
+
+import org.junit.jupiter.api.Test;
+
+public class Contest458wTask1Tests {
+
+    Contest458wTask1 task1 = new Contest458wTask1();
+
+    @Test
+    public void test() {
+        System.out.println(task1.processStr("a#b%*"));
+    }
+}
